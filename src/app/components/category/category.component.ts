99a115a3531df7b10e113825865ebb81219ac6ed
Alparslan <alparslanaydgn02@gmail.com>
2022-12-05 18:27:29 +0300

    ListResponseModel added

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Category } from 'src/app/models/category';
+import { CategoryService } from 'src/app/services/category.service';
 
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
   styleUrls: ['./category.component.css']
 })
-export class CategoryComponent {
+export class CategoryComponent implements OnInit{
+  categories: Category[]=[];
+  dataLoaded=false;
+
+  constructor(private categoryService:CategoryService) {}
+
+
+  ngOnInit(): void {
+    this.getCategories();
+  }
+
+  getCategories() { //asenkron yapı
+    this.categoryService.getCategories().subscribe(response=>{
+      this.categories=response.data;
+      this.dataLoaded=true;
+    })
+  }
 
 }
