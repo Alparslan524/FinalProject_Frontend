c1595488e9db3516fce00b8693e305629a773393
Alparslan <alparslanaydgn02@gmail.com>
2022-12-07 16:17:24 +0300

    Added to CART

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+
 import { Product } from 'src/app/models/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -15,7 +18,10 @@ export class ProductComponent implements OnInit {
   filterText="";
   
   constructor(private productService:ProductService, 
-    private activatedRoute:ActivatedRoute) {}
+    private activatedRoute:ActivatedRoute, 
+    private toastrService:ToastrService,
+    private cartService:CartService
+    ) {}
 
   ngOnInit(): void {
     this.activatedRoute.params.subscribe(params=>{
@@ -40,6 +46,17 @@ export class ProductComponent implements OnInit {
     })
   }
 
+
+  addToCart(product:Product){
+    if (product.productID===1) {
+      this.toastrService.error("Error","This product cannot be added to the cart(deneme amaçlı)");
+    } else {
+      this.toastrService.success("Added to Cart",product.productName)
+      this.cartService.addToCart(product);
+    }
+    
+    
+  }
 
 
 }
